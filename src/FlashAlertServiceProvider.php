1646494ffdd0a2a007c51c3dd859cd792c5ac4f1
Erik Galloway <emg5478@psu.edu>
2016-06-27 18:24:54 -0500

    Flip abstract vs concrete bind in service provider

--- src/FlashAlertServiceProvider.php
+++ src/FlashAlertServiceProvider.php
@@ -29,7 +29,7 @@ class FlashAlertServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(SessionStore::class, SessionStoreContract::class);
+        $this->app->bind(SessionStoreContract::class, SessionStore::class);
 
         $this->app->singleton('alert', function(){
             return $this->app->make(Alert::class);
