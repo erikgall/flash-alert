55c9be5703ebebc949232126d598ab41ba27be10
Erik Galloway <emg5478@psu.edu>
2016-06-28 11:51:11 -0500

    Change the interval vue prop to autohide in view file

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -1,9 +1,9 @@
 @if(session()->has('alert'))
 
-    <alert-static :show.sync="true"
+    <flash-alert :show.sync="true"
                   icon="{{ session()->has('alert.icon') ? session('alert.icon') : 'fa-bell-o' }}"
                   type="{{ session('alert.type') }}"
                   message="{{ session('alert.message') }}"
-                  autohide="{{ session()->has('alert.autohide') ? session('alert.autohide') : '0' }}"></alert-static>
+                  autohide="{{ session()->has('alert.autohide') ? session('alert.autohide') : '0' }}"></flash-alert>
 
 @endif
