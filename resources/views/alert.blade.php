04583015684d11489642169884d1378e011d8b05
Erik Galloway <emg5478@psu.edu>
2016-06-28 12:29:56 -0500

    Change autohide from inserting a string instead of expected number

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -4,6 +4,6 @@
                   icon="{{ session()->has('alert.icon') ? session('alert.icon') : 'fa-bell-o' }}"
                   type="{{ session('alert.type') }}"
                   message="{{ session('alert.message') }}"
-                  autohide="{{ session()->has('alert.autohide') ? session('alert.autohide') : '0' }}"></flash-alert>
+                  autohide="{{ session()->has('alert.autohide') ? session('alert.autohide') : 0 }}"></flash-alert>
 
 @endif
